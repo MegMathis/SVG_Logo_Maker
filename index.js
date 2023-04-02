56e6f8733b9d0a6f3bfc846347aaf2038d00f5a3
MegMathis <mcdonough.megan25@gmail.com>
2023-04-02 15:30:12 -0400

    input_questions

--- index.js
+++ index.js
@@ -2,3 +2,36 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+const questions = [
+  {
+    type: "maxlength-input",
+    name: "text",
+    message: "Please enter three characters for your logo text.",
+    maxLength: 3,
+    validate: (input) => {
+      if (input.length <= 3) {
+        return true;
+      } else {
+        return "Text is too long, text must be three characters or less";
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "text_color",
+    message: "Please enter the color you want your text.",
+  },
+
+  {
+    type: "list",
+    name: "shape",
+    message: "Please choose a shape for your logo.",
+    choices: ["Circle", "Triangle", "Square"],
+  },
+  {
+    type: "input",
+    name: "shape_color",
+    message: "Please enter the color you want your shape",
+  },
+];
